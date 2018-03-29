0ac3e5cedb0dcdaa57d8f6ade1d699c5a6dfed0e
steve gorelik <goksteve@yahoo.com>
2018-03-29 10:48:36 -0400

    SG OK

---
+++ Projects/DataWarehouse/CDW/STG_VISIT_SEG_LOCATIONS.sql
@@ -0,0 +1,48 @@
+EXEC dbm.drop_tables('STG_VISIT_SEGMENT_LOCATIONS');
+
+CREATE TABLE stg_visit_segment_locations
+(
+ network                     CHAR(3 BYTE) NOT NULL,
+ visit_id                    NUMBER(12) NOT NULL,
+ visit_segment_number        NUMBER(12) NOT NULL,
+ location_id                 VARCHAR2(12 BYTE) NOT NULL,
+ activation_time             DATE,
+ visit_number                VARCHAR2(40 BYTE),
+ visit_type_id               NUMBER(12),
+ visit_subtype_id            NUMBER(12),
+ financial_class_id          NUMBER(12),
+ admitting_emp_provider_id   NUMBER(12),
+ diagnosis                   VARCHAR2(200 BYTE),
+ visit_service_type_id       NUMBER(12),
+ facility_id                 NUMBER(12),
+ arrival_mode_id             NUMBER(12),
+ arrival_mode_string         VARCHAR2(100 BYTE),
+ admit_event_id              NUMBER(12),
+ last_edit_time              DATE,
+ invalid_conversion_flag     VARCHAR2(7 BYTE),
+ accident_on_job             VARCHAR2(5 BYTE),
+ load_dt                     DATE DEFAULT TRUNC(SYSDATE) NOT NULL,
+ cid                         NUMBER(14)
+)
+PARTITION BY LIST (network)
+ SUBPARTITION BY HASH (visit_id)
+  SUBPARTITIONS 16
+ (PARTITION cbn VALUES ('CBN'),
+  PARTITION gp1 VALUES ('GP1'),
+  PARTITION gp2 VALUES ('GP2'),
+  PARTITION nbn VALUES ('NBN'),
+  PARTITION nbx VALUES ('NBX'),
+  PARTITION qhn VALUES ('QHN'),
+  PARTITION sbn VALUES ('SBN'),
+  PARTITION smn VALUES ('SMN'));
+
+GRANT SELECT ON stg_visit_segment_locations TO PUBLIC;
+
+CREATE UNIQUE INDEX pk_stg_visit_seg_loc
+ ON stg_visit_segment_locations(network, visit_id, visit_segment_number,location_id)  LOCAL  PARALLEL 32;
+
+ALTER INDEX pk_stg_visit_seg_loc NOPARALLEL;
+
+ALTER TABLE stg_visit_segment_locations
+ ADD CONSTRAINT pk_stg_visit_seg_loc PRIMARY KEY(network, visit_id, visit_segment_number, location_id)
+     USING INDEX pk_stg_visit_seg_loc;
---
+++ Projects/DataWarehouse/CDW/V_STG_VISIT_SEGMENT_LOCATIONS.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE VIEW v_stg_visit_segment_locations AS
+ SELECT
+  s.network,
+  s.visit_id,
+  s.visit_segment_number,
+ nvl( l.location_id,'N/A') location_id,
+  s.activation_time,
+  s.visit_number,
+  s.visit_type_id,
+  s.visit_subtype_id,
+  s.financial_class_id,
+  s.admitting_emp_provider_id,
+  s.diagnosis,
+  s.visit_service_type_id,
+  s.facility_id,
+  s.arrival_mode_id,
+  s.arrival_mode_string,
+  s.admit_event_id,
+  s.last_edit_time,
+  s.invalid_conversion_flag,
+  s.accident_on_job,
+  s.cid AS visit_seg_cid
+ FROM
+  visit_segment s
+  LEFT JOIN visit_segment_visit_location l
+   ON l.network = s.network AND l.visit_id = s.visit_id AND l.visit_segment_number = s.visit_segment_number
