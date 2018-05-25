6d6d1dd80ea7e3949e888f6f55e5b7589cbfd4c6
steve gorelik <goksteve@yahoo.com>
2018-05-25 11:49:25 -0400

    SG

--- Projects/DataWarehouse/CDW/MV_FACT_DAILY_VISITS_STATS;.sql
+++ Projects/DataWarehouse/CDW/MV_FACT_DAILY_VISITS_STATS;.sql
@@ -2,9 +2,10 @@ DROP MATERIALIZED VIEW mv_fact_daily_visits_stats;
 
 CREATE MATERIALIZED VIEW mv_fact_daily_visits_stats
  BUILD IMMEDIATE
- REFRESH COMPLETE
-  START WITH TRUNC(SYSDATE) + 23/ 24
-  NEXT (TRUNC(SYSDATE) +1)+ 23 / 24 AS
+ REFRESH
+  COMPLETE
+  START WITH TRUNC(SYSDATE) + 23 / 24
+  NEXT (TRUNC(SYSDATE) + 1) + 23 / 24 AS
  SELECT
   a.network,
   a.visit_id,
@@ -14,27 +15,58 @@ CREATE MATERIALIZED VIEW mv_fact_daily_visits_stats
   a.visit_type_id,
   a.visit_type,
   a.medicaid_ind,
-  a.patient_id,
+  CAST(a.patient_id AS VARCHAR2(256)) patient_id,
   a.mrn,
-  a.pat_lname,
-  a.pat_fname,
+  a.pat_lname || ', ' || a.pat_fname AS patient_name,
   a.sex,
-  trunc(a.birthdate) as birthdate ,
+  TRUNC(a.birthdate) AS birthdate,
   a.age,
-  a.admission_dt,
-  a.discharge_dt,
-  NVL(b.asthma_ind,0) AS asthma_ind,
-  NVL(b.bh_ind,0) AS bh_ind,
-  NVL(b.breast_cancer_ind,0) AS breast_cancer_ind,
-  NVL(b.diabetes_ind,0) AS diabetes_ind,
-  NVL(b.heart_failure_ind,0) AS heart_failure_ind,
-  NVL(b.hypertansion_ind,0) AS hypertansion_ind,
-  NVL(b.kidney_diseases_ind,0) AS kidney_diseases_ind,
-  NVL(b.pregnancy_ind,0) AS pregnancy_ind,
-      b.pregnancy_onset_dt
+  TRUNC(a.admission_dt) AS admission_dt,
+  TRUNC(a.discharge_dt) AS discharge_dt,
+  NVL(b.asthma_ind, 0) AS asthma_ind,
+  NVL(b.bh_ind, 0) AS bh_ind,
+  NVL(b.breast_cancer_ind, 0) AS breast_cancer_ind,
+  NVL(b.diabetes_ind, 0) AS diabetes_ind,
+  NVL(b.heart_failure_ind, 0) AS heart_failure_ind,
+  NVL(b.hypertansion_ind, 0) AS hypertansion_ind,
+  NVL(b.kidney_diseases_ind, 0) AS kidney_diseases_ind,
+  NVL(b.pregnancy_ind, 0) AS pregnancy_ind,
+  TRUNC(b.pregnancy_onset_dt) pregnancy_onset_dt,
+  'QCPR' AS source
  FROM
   fact_daily_visits_stats a
   LEFT JOIN fact_patient_metric_diag b ON b.network = a.network AND a.patient_id = b.patient_id
-where  admission_dt < trunc(sysdate);
- 
-grant select on MV_FACT_DAILY_VISITS_STATS to public
+ WHERE
+  admission_dt < TRUNC(SYSDATE)
+ UNION
+ SELECT
+  network,
+  visit_id,
+  NULL visit_number,
+  facility_key,
+  facility_name,
+  NULL visit_type_id,
+  visit_type,
+  NULL medicaid_ind,
+  patient_id,
+  mrn,
+  patient_name,
+  sex,
+  TRUNC(CAST(birth_date AS DATE)) birth_date,
+  age,
+  TRUNC(CAST(admission_dt AS DATE)) admission_dt,
+  TRUNC(CAST(discharge_dt AS DATE)) discharge_dt,
+  asthma_ind,
+  bh_ind,
+  breast_cancer_ind,
+  diabetes_ind,
+  heart_failure_ind,
+  hypertansion_ind,
+  kidney_diseases_ind,
+  pregnancy_ind,
+  TRUNC(pregnancy_onset_dt),
+  'EPIC' AS source
+ FROM
+  epic_clarity.epic_fact_daily_visits_stats;
+
+GRANT SELECT ON mv_fact_daily_visits_stats TO PUBLIC
--- Projects/DataWarehouse/CDW/V_REF_PATIENT_SECONDARY_MRN.sql
+++ Projects/DataWarehouse/CDW/V_REF_PATIENT_SECONDARY_MRN.sql
@@ -1,25 +1,42 @@
-CREATE OR REPLACE VIEW V_REF_PATIENT_SECONDARY_MRN AS
- SELECT /*+ PARALLEL (32) */
-  network,
-  patient_id,
-  secondary_number AS second_mrn,
-  CASE
-   WHEN (network = 'GP1' AND secondary_nbr_type_id = 13) THEN 1
-   WHEN (network = 'GP1' AND secondary_nbr_type_id = 11) THEN 2
-   WHEN (network = 'GP1' AND secondary_nbr_type_id = 12) THEN 3
-   WHEN (network = 'CBN' AND secondary_nbr_type_id = 12) THEN 4
-   WHEN (network = 'CBN' AND secondary_nbr_type_id = 13) THEN 5
-   WHEN (network = 'NBN' AND secondary_nbr_type_id = 9) THEN 2
-   WHEN (network = 'NBX' AND secondary_nbr_type_id = 11) THEN 2
-   WHEN (network = 'QHN' AND secondary_nbr_type_id = 11) THEN 2
-   WHEN (network = 'SBN' AND secondary_nbr_type_id = 11) THEN 1
-   WHEN (network = 'SMN' AND secondary_nbr_type_id = 11) THEN 2
-   WHEN (network = 'SMN' AND secondary_nbr_type_id = 13) THEN 7
-   WHEN (network = 'SMN' AND secondary_nbr_type_id = 14) THEN 8
-   WHEN (network = 'SMN' AND secondary_nbr_type_id = 17) THEN 9
-   ELSE NULL
-  END
-   AS facility_id
+CREATE OR REPLACE VIEW v_ref_patient_secondary_mrn AS
+ WITH sec_nbr AS
+       (SELECT /*+ materialize) */
+         network,
+         patient_id,
+         secondary_number AS second_mrn,
+         CASE
+          WHEN (network = 'GP1' AND secondary_nbr_type_id = 13) THEN 1
+          WHEN (network = 'GP1' AND secondary_nbr_type_id = 11) THEN 2
+          WHEN (network = 'GP1' AND secondary_nbr_type_id = 12) THEN 3
+          WHEN (network = 'CBN' AND secondary_nbr_type_id = 12) THEN 4
+          WHEN (network = 'CBN' AND secondary_nbr_type_id = 13) THEN 5
+          WHEN (network = 'NBN' AND secondary_nbr_type_id = 9) THEN 2
+          WHEN (network = 'NBX' AND secondary_nbr_type_id = 11) THEN 2
+          WHEN (network = 'QHN' AND secondary_nbr_type_id = 11) THEN 2
+          WHEN (network = 'SBN' AND secondary_nbr_type_id = 11) THEN 1
+          WHEN (network = 'SMN' AND secondary_nbr_type_id = 11) THEN 2
+          WHEN (network = 'SMN' AND secondary_nbr_type_id = 13) THEN 7
+          WHEN (network = 'SMN' AND secondary_nbr_type_id = 14) THEN 8
+          WHEN (network = 'SMN' AND secondary_nbr_type_id = 17) THEN 9
+          ELSE NULL
+         END
+          AS facility_id
+        FROM
+         patient_secondary_number
+        WHERE
+         secondary_nbr_type_id IN (11,
+                                   12,
+                                   13,
+                                   9,
+                                   14,
+                                   17))
+ SELECT /*+ parallel (32) */
+  distinct n.network,
+  n.patient_id,
+  TRIM(n.second_mrn) second_mrn,
+  f.facility_key,
+  n.facility_id
  FROM
-  patient_secondary_number
- WHERE  secondary_nbr_type_id IN (11,12,13,9,14,17)
+  dim_hc_facilities f JOIN sec_nbr n ON n.network = f.network AND n.facility_id = f.facility_id
+ WHERE
+  n.facility_id IS NOT NULL AND (n.second_mrn IS NOT NULL AND TRIM(n.second_mrn) <> '*')
