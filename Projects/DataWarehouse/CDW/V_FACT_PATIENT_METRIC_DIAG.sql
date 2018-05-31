2bdd6712981b4e70158c2129758698de630e6de7
steve gorelik <goksteve@yahoo.com>
2018-05-31 10:29:54 -0400

    SG OK

--- Projects/DataWarehouse/CDW/V_FACT_PATIENT_METRIC_DIAG.sql
+++ Projects/DataWarehouse/CDW/V_FACT_PATIENT_METRIC_DIAG.sql
@@ -22,18 +22,20 @@ CREATE OR REPLACE VIEW V_FACT_PATIENT_METRIC_DIAG AS
   WHERE
    cr.criterion_id IN (1,3,6,7,9,11,17,18,21,27,30,31,32,36,37,38,39,48,49,50,51,52,53,57,58,59,60,63,65,70,71,73) --and INCLUDE_EXCLUDE_IND  = 'I'
  ),
-
 pat_smoker
 AS
-(
-  SELECT
-   network, patient_id, DECODE(smoker_flag, 'F', 1, 0) AS smoker_ind
-  FROM
-   dim_patients
-  WHERE
-   current_flag = 1 AND smoker_flag is not null 
-  
-),
+  (
+     SELECT
+     network,
+     patient_id,
+    'smoker' AS diag_type_ind,
+     NULL onset_date
+    FROM
+     dim_patients
+    WHERE
+     current_flag = 1 AND smoker_flag IS NOT NULL
+    AND network = SYS_CONTEXT('CTX_CDW_MAINTENANCE', 'NETWORK')
+  ),
 rslt_pat
 AS
 (
@@ -52,14 +54,28 @@ AS
     fact_patient_diagnoses s JOIN meta_diag d ON s.diag_code = d.VALUE
     WHERE
     problem_status = 'active'
-    AND s.network = SYS_CONTEXT('CTX_CDW_MAINTENANCE', 'NETWORK')
+   AND s.network = SYS_CONTEXT('CTX_CDW_MAINTENANCE', 'NETWORK')
   )
   WHERE cnt = 1
-),
-TMP_PIVOT
+)
+,
+TMP_RES
 AS
 (
- SELECT /*+ materialize */
+ SELECT  network, patient_id, diag_type_ind, onset_date
+    FROM rslt_pat
+    WHERE include_exclude_ind = 'I'
+    AND (network, patient_id, diag_type_ind) NOT IN 
+              ( 
+              SELECT network, patient_id, diag_type_ind FROM
+                   rslt_pat  WHERE include_exclude_ind = 'E'
+               )
+     UNION
+          SELECT network, patient_id, diag_type_ind, onset_date FROM pat_smoker
+)
+
+
+ SELECT /*+ Parallel (48) */
  network,
  patient_id,
  asthma_ind,
@@ -67,21 +83,13 @@ AS
  breast_cancer_ind,
  diabetes_ind,
  heart_failure_ind,
- hypertansion_ind,
+ hypertension_ind,
  kidney_diseases_ind,
+ smoker_ind,
  pregnancy_ind,
  pregnancy_onset_dt
 FROM
-  (
-  SELECT
-  network, patient_id, diag_type_ind,onset_date
-  FROM   rslt_pat
-  WHERE include_exclude_ind = 'I'
-AND ( network, patient_id, diag_type_ind) not IN(SELECT
-  network, patient_id, diag_type_ind
-  FROM rslt_pat
-  WHERE include_exclude_ind = 'E')
-  )
+ TMP_RES
 
 PIVOT
  (
@@ -94,50 +102,13 @@ PIVOT
       'breast_cancer' AS breast_cancer,
       'diabetes' AS diabetes,
       'heart_failure' AS heart_failure,
-      'hypertansion' AS hypertansion,
+      'hypertension' AS hypertension,
       'kidney_diseases' AS kidney_diseases,
+      'smoker' as smoker,
       'pregnancy' AS pregnancy
 
     )
  )
-)
- 
 
 
 
-SELECT /*+ PARALLEL (48) */
- NVL(a.network, b.network) AS network,
- NVL(a.patient_id, b.patient_id) AS patient_id,
- NVL(a.asthma_ind, 0) asthma_ind,
- NVL(a.bh_ind, 0) bh_ind,
- NVL(a.breast_cancer_ind, 0) breast_cancer_ind,
- NVL(a.diabetes_ind, 0) diabetes_ind,
- NVL(a.heart_failure_ind, 0) heart_failure_ind,
- NVL(a.hypertansion_ind, 0) hypertansion_ind,
- NVL(a.kidney_diseases_ind, 0) kidney_diseases_ind,
- NVL(b.smoker_ind,0) AS smoker_ind,
- NVL(a.pregnancy_ind, 0) pregnancy_ind,
- a.pregnancy_onset_dt
-FROM
- pat_smoker b FULL OUTER  JOIN tmp_pivot a ON a.network = b.network AND a.patient_id = b.patient_id
-
-
-
-
-
---UNION
---SELECT
--- a.network,
--- a.patient_id,
--- NVL(a.asthma_ind, 0) asthma_ind,
--- NVL(a.bh_ind, 0) bh_ind,
--- NVL(a.breast_cancer_ind, 0) breast_cancer_ind,
--- NVL(a.diabetes_ind, 0) diabetes_ind,
--- NVL(a.heart_failure_ind, 0) heart_failure_ind,
--- NVL(a.hypertansion_ind, 0) hypertansion_ind,
--- NVL(a.kidney_diseases_ind, 0) kidney_diseases_ind,
--- DECODE(b.smoker_flag, 'F', 1, 0) AS smoker_ind,
--- NVL(a.pregnancy_ind, 0) pregnancy_ind,
--- a.pregnancy_onset_dt
---FROM
--- tmp_pivot a LEFT  JOIN pat_smoker b ON a.network = b.network AND a.patient_id = b.patient_id
