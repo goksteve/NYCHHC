d3a67d5c60c7e8664ac613ce788d91a12692c129
Ganesh Kolluru <ganeshkolluru@gmail.com>
2018-08-23 12:31:52 -0400

    gk

--- Projects/DataWarehouse/CDW/V_DIM_DISCHARGE_TYPES.sql
+++ Projects/DataWarehouse/CDW/V_DIM_DISCHARGE_TYPES.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE VIEW v_dim_discharge_types AS
   p.network,
   p.visit_type_id,
   p.discharge_type_id,
-  p.name,
+  p.name as discharge_type_name,
   p.facility_id
  FROM
   discharge_type p;
